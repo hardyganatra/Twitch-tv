cf81e7c6fd31a24783fba71321ebf1e68bb4c04c
Hardik Ganatra <hardik.ganatra@lntinfotech.com>
2020-01-04 11:30:12 +0530

    routes wired

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import StreamCreate from "./Components/Streams/StreamCreate";
+import StreamDelete from "./Components/Streams/StreamDelete";
+import StreamEdit from "./Components/Streams/StreamEdit";
+import StreamList from "./Components/Streams/StreamList";
+import StreamShow from "./Components/Streams/StreamShow";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>This will be our Header</h1>
+			<Router>
+				<Route path="/" exact component={StreamList}></Route>
+				<Route path="/streams/new" component={StreamCreate}></Route>
+				<Route path="/streams/edit" component={StreamEdit}></Route>
+				<Route path="/streams/delete" component={StreamDelete}></Route>
+				<Route path="/streams/show" component={StreamShow}></Route>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
